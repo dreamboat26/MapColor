9f366c34edd4769581ceee020fbdb94db9ecda2e
Mahule Roy <125608791+dreamboat26@users.noreply.github.com>
2023-04-02 01:30:06 +0530

    Update map.py

--- map.py
+++ map.py
@@ -1,39 +1,77 @@
-colors = ['Orange','Yellow','Green']
+import matplotlib.pyplot as plt
+# Define the map of Australia as a dictionary with state names as keys and neighbors as values
+australia = {
+    "WA": ["SA", "NT"],
+    "NT": ["WA", "SA", "QL"],
+    "SA": ["WA", "NT", "QL", "NSW", "VI"],
+    "QL": ["NT", "SA", "NSW"],
+    "NSW": ["QL", "SA", "VI"],
+    "VI": ["NSW", "SA"],
+}
 
-states = ['Western_Australia', 'Northern_Territory', 'Southern_Australia', 'Queensland', 'New_South_Wales', 'Victoria','Tasmania']
-#the states of Australia are stored in a list called states. We can also store it as an adjacency matrix for bigger maps
-neighbors = {} #dict to store the neighbours
-neighbors['Western_Australia'] = ['Northern_Territory', 'Southern_Australia']
-neighbors['Northern_Territory'] = ['Western_Australia', 'Southern_Australia', 'Queensland']
-neighbors['Southern_Australia'] = ['Western_Australia', 'Northern_Territory', 'Queensland', 'New_South_Wales', 'Victoria']
-neighbors['Queensland'] = ['Northern_Territory', 'Southern_Australia', 'New_South_Wales']
-neighbors['New_South_Wales'] = ['Queensland', 'Southern_Australia', 'Victoria']
-neighbors['Victoria'] = ['Southern_Australia', 'New_South_Wales']
-neighbors['Tasmania'] = []      #Tasmania doesn't have any neighbour and is not attached to any other node in the graph
+# Define the domain of colors
+colors = ["red", "green", "blue"]
 
-colors_of_states = {}    #dict 
-def find(state, color):
-    for neighbor in neighbors.get(state):                     #iterates over the neighbours and checks that no adjacent area has same color
-        color_of_neighbor = colors_of_states.get(neighbor)
-        if color_of_neighbor == color:
+# Define a function to check if the current state assignment is consistent with the constraints
+def is_consistent(state, color, assignment):
+    for neighbor in australia[state]:
+        if neighbor in assignment and assignment[neighbor] == color:
             return False
     return True
 
-def find_color_for_state(state): #after find returns True then we return that color to the main function meaning that the adjacent area doen't have that color. For Southern_Australia it will check with all its adjacent area and then only assign the color to it
+# Define a recursive function to search for a solution
+def backtrack_search(assignment):
+    # If all states are assigned, return the solution
+    if len(assignment) == len(australia):
+        return assignment
+    
+    # Select an unassigned state
+    unassigned_states = [state for state in australia.keys() if state not in assignment]
+    state = unassigned_states[0]
+    
+    # Try each possible value for the selected state
     for color in colors:
-        if find(state, color):
-            return color
+        # Check if the current state assignment is consistent with the constraints
+        if is_consistent(state, color, assignment):
+            # Add the current state assignment to the partial solution
+            assignment[state] = color
+            # Recursively search for a solution with the updated partial solution
+            result = backtrack_search(assignment)
+            # If a solution is found, return it
+            if result is not None:
+                return result
+            # Otherwise, backtrack and try the next possible value for the selected state
+            del assignment[state]
+    
+    # If no solution is found, return None
     return None
 
-def main():
-    for state in states:
-        color = find_color_for_state(state)
-        if color is None:
-            print("No solution exists.")       #for times when the constraints don't get satisfied and the map coloring for that graph is not possible
-            return
-        colors_of_states[state] = color
-    for state, color in colors_of_states.items():
-        print(f"{state}: {color}")
-    
-if __name__ == '__main__':
-    main()
+# Solve the problem and print the solution
+solution = backtrack_search({})
+print(solution)
+
+# Define the coordinates of the nodes for plotting
+coordinates = {
+    "WA": (0, 2),
+    "NT": (1, 1),
+    "SA": (2, 2),
+    "QL": (1, 0),
+    "NSW": (3, 1),
+    "VI": (4, 2),
+}
+
+# Plot the graph
+fig, ax = plt.subplots()
+for state, neighbors in australia.items():
+    for neighbor in neighbors:
+        x1, y1 = coordinates[state]
+        x2, y2 = coordinates[neighbor]
+        ax.plot([x1, x2], [y1, y2], color="gray")
+    x, y = coordinates[state]
+    circle = plt.Circle((x, y), radius=0.4, color=solution[state])
+    ax.add_patch(circle)
+    ax.text(x, y, state, ha="center", va="center")
+ax.set_aspect("equal")
+ax.set_xlim([-0.5, 4.5])
+ax.set_ylim([-0.5, 2.5])
+plt.show()
